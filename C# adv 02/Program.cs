30e6dcc13e5b7a26e3e232f0529e2d4ddd2f4716
Mazen Fady <mazenfady85@gmail.com>
2025-07-31 03:38:39 +0300

    Add project files.

---
+++ C# adv 02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Demo 
+{
+    internal class Program 
+    {
+        #region Q1
+
+        #endregion
+        internal static void Main(string[] args) {
+
+
+
+    }
+
+}
